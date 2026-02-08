96e4ea536879e3e08941514e73d51b0fb8489a3b
lyb <rizenis@naver.com>
2026-02-09 00:43:55 +0900

    astar 삭제

--- Packages/com.arongranberg.astar/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("AstarPathfindingProjectEditor")]
-#if UNITY_2023_1_OR_NEWER
-[assembly: Unity.Properties.GeneratePropertyBagsForAssembly]
-#endif
